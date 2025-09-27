5ce83c4544d4b04f3f161537cfd5bb736b25a7bd
mastervieira <javatang46@gmail.com>
2025-09-27 15:38:17 +0100

    Created filter search

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 
@@ -20,6 +20,39 @@ def index(request):
         'contact/index.html',
         context,
     )
+
+# https://docs.djangoproject.com/en/5.2/ref/models/querysets/#field-lookups
+
+def search(request):
+    serach_value = request.GET.get('q','').strip()
+    
+    if serach_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(frist_name__icontains=serach_value) |
+            Q(last_name__icontains=serach_value) |
+            Q(id__icontains=serach_value) |
+            Q(phone__icontains=serach_value) |
+            Q(email__icontains=serach_value) 
+            )\
+        .order_by('-id')
+    # print(contacts.query)
+    
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search -'
+    }
+    
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+
 
 def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
